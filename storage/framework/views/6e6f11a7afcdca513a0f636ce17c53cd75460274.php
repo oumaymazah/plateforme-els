4cfa43f2ee006e7e74dcf659948c10e8cef051e1
iheb nachet <inachet3@gmail.com>
2024-02-10 13:34:12 +0100

    auth done

---
+++ storage/framework/views/6e6f11a7afcdca513a0f636ce17c53cd75460274.php
@@ -0,0 +1,34 @@
+<div class="page-main-header">
+  <div class="main-header-right row m-0">
+    <div class="main-header-left">
+      <div class="logo-wrapper"><a href="<?php echo e(route('index')); ?>"><img class="img-fluid" src="<?php echo e(asset('assets/images/logo/logo.png')); ?>" alt=""></a></div>
+      <div class="dark-logo-wrapper"><a href="<?php echo e(route('index')); ?>"><img class="img-fluid" src="<?php echo e(asset('assets/images/logo/dark-logo.png')); ?>" alt=""></a></div>
+      <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle">    </i></div>
+    </div>
+
+    <div class="nav-right col pull-right right-menu p-0">
+      <ul class="nav-menus">
+        <li><a class="text-dark" href="#!" onclick="javascript:toggleFullScreen()"><i data-feather="maximize"></i></a></li>
+       
+        <li>
+            <div class="mode"><i class="fa fa-moon-o"></i></div>
+        </li>
+       
+        <li class="onhover-dropdown p-0">
+          <a class="btn btn-primary-light" href="<?php echo e(route('logout')); ?>"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <?php echo e(__('Logout')); ?>
+
+                                    </a>
+
+                                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" class="d-none">
+                                        <?php echo csrf_field(); ?>
+                                    </form>
+        </li>
+      </ul>
+    </div>
+    <div class="d-lg-none mobile-toggle pull-right w-auto"><i data-feather="more-horizontal"></i></div>
+  </div>
+</div>
+<?php /**PATH /Users/iheb/Herd/alibaba-tirage/resources/views/layouts/admin/partials/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/835e946c28c5eb970e41894a78576f0569898989.php
@@ -0,0 +1,97 @@
+<?php $__env->startSection('title'); ?>login
+ <?php echo e($title); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startPush('css'); ?>
+<?php $__env->stopPush(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <section>
+	    <div class="container-fluid">
+	        <div class="row">
+	            <div class="col-xl-7"><img class="bg-img-cover bg-center" src="<?php echo e(asset('assets/images/login/2.jpg')); ?>" alt="looginpage" /></div>
+	            <div class="col-xl-5 p-0">
+	                <div class="login-card">
+	                    <form class="theme-form login-form" method="POST" action="<?php echo e(route('login')); ?>">
+							<?php echo csrf_field(); ?>
+	                        <h4>Login</h4>
+	                        <h6>Welcome back! Log in to your account.</h6>
+	                        <div class="form-group">
+	                            <label>Email Address</label>
+	                            <div class="input-group">
+	                                <span class="input-group-text"><i class="icon-email"></i></span>
+	                                <input class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" type="email" required="" placeholder="Test@gmail.com" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus />
+
+									<?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong><?php echo e($message); ?></strong>
+                                    </span>
+                                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+	                            </div>
+	                        </div>
+	                        <div class="form-group">
+	                            <label>Password</label>
+	                            <div class="input-group">
+	                                <span class="input-group-text"><i class="icon-lock"></i></span>
+	                                <input  type="password" name="password" required="" placeholder="*********"  class="form-control <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="password" required autocomplete="current-password"/>
+	                                <div class="show-hide"><span class="show"> </span></div>
+									<?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong><?php echo e($message); ?></strong>
+                                    </span>
+                                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+	                            </div>
+	                        </div>
+	                        <div class="form-group">
+	                            <div class="checkbox">
+	                                <input type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?> />
+	                                <label class="text-muted" for="checkbox1">Remember password</label>
+	                            </div>
+	                        </div>
+	                        <div class="form-group">
+	                            <button class="btn btn-primary btn-block" type="submit">Sign in</button>
+	                        </div>
+	                       
+	                        
+	                    </form>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+
+	
+    <?php $__env->startPush('scripts'); ?>
+    <?php $__env->stopPush(); ?>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('admin.authentication.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/iheb/Herd/alibaba-tirage/resources/views/admin/authentication/login_one.blade.php ENDPATH**/ ?>
